ecabe9e20f2c84cadb9391e9983e3ffbffc396ff
sadiya <ssadiya.shaikh02@gmail.com>
2024-04-30 17:18:52 +0530

    Main commit

--- app/Http/Controllers/stripePaymentController.php
+++ app/Http/Controllers/stripePaymentController.php
@@ -2,7 +2,9 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Session;
-use Stripe;
+use Stripe\Stripe;
+use Stripe\Exception\CardException;
+use Stripe\PaymentIntent;
 class stripePaymentController extends Controller
 {
     /**
@@ -22,16 +24,27 @@ class stripePaymentController extends Controller
      */
     public function stripePost(Request $request)
     {
-        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        // Set your Stripe API key
+        Stripe::setApiKey(env('STRIPE_SECRET'));
 
-        Stripe\Charge::create([
-            "amount" => 100 * 100, // Amount in cents
-            "currency" => "usd",
-            "source" => $request->stripeToken,
-            "description" => "Test payment from itsolutionstuff.com."
-        ]);
+        try {
+            // Create a payment intent
+            $paymentIntent = PaymentIntent::create([
+                'amount' => 100 * 100, // Amount in cents
+                'currency' => 'usd',
+                'payment_method' => $request->stripePaymentMethod,
+                'confirm' => true,
+                'description' => 'Test payment from your website.',
+                'return_url' => route('home')
+            ]);
 
-        Session::flash('success', 'Payment successful!');
-        return back();
+            // Payment successful, flash success message
+            Session::flash('success', 'Payment successful!');
+        } catch (CardException $e) {
+            // Payment failed, flash error message
+            Session::flash('error', $e->getMessage());
+        }
+
+        return redirect()->back();
     }
 }
--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -1,6 +1,7 @@
 @extends('front.layouts.app')
+
 @section('content')
-<section class="section-5 pt-3 pb-3 mb-3 bg-white">
+    <section class="section-5 pt-3 pb-3 mb-3 bg-white">
         <div class="container">
             <div class="light-font">
                 <ol class="breadcrumb primary-color mb-0">
@@ -11,72 +12,132 @@
         </div>
     </section>
 
-    <section class=" section-10">
+    <section class="section-10">
         <div class="container">
             @if(Session::has('success'))
-           <div class="alert alert-success">
-            {{ Session::get('success')}}
-           </div>
+                <div class="alert alert-success">
+                    {{ Session::get('success') }}
+                </div>
             @endif
             @if(Session::has('error'))
-           <div class="alert alert-danger">
-            {{ Session::get('error')}}
-           </div>
+                <div class="alert alert-danger">
+                    {{ Session::get('error') }}
+                </div>
             @endif
-            <div class="login-form">    
-            <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
-                        @csrf
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Name on Card</label> <input
-                                    class='form-control' size='4' type='text'>
-                            </div>
+            <div class="login-form">
+                <form role="form" action="{{ route('stripe.post') }}" method="post" id="payment-form">
+                    @csrf
+                    <div class='form-row row'>
+                        <div class='col-xs-12 form-group required'>
+                            <label class='control-label'>Name on Card</label>
+                            <input class='form-control' size='4' type='text' name="name_on_card">
                         </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group  required'>
-                                <label class='control-label'>Card Number</label> 
-                                <input autocomplete='off' class='form-control card-number' size='20' type='text'>
-                            </div>
+                    </div>
+
+                    <div class='form-row row'>
+                        <div class='col-xs-12 form-group required'>
+                            <label class='control-label'>Card Number</label>
+                            <div id="card-number" class="form-control"></div>
                         </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label'>CVC</label> <input autocomplete='off'
-                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Exp Month</label> <input
-                                    class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Exp Year</label> <input
-                                    class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text'>
-                            </div>
+                    </div>
+
+                    <div class='form-row row'>
+                        <div class='col-xs-12 col-md-4 form-group cvc required'>
+                            <label class='control-label'>CVC</label>
+                            <div id="card-cvc" class="form-control"></div>
                         </div>
-    
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Please correct the errors and try
-                                    again.</div>
-                            </div>
+                        <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>Exp Month</label>
+                            <div id="card-expiry-month" class="form-control"></div>
                         </div>
-    
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($100)</button>
-                            </div>
+                        <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>Exp Year</label>
+                            <div id="card-expiry-year" class="form-control"></div>
                         </div>
-                            
-                    </form>
+                    </div>
+
+                    <div class='form-row row'>
+                        <div class='col-md-12 error form-group hide'>
+                            <div class='alert-danger alert'>Please correct the errors and try again.</div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <button id="card-button" class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($100)</button>
+                        </div>
+                    </div>
+
+                </form>
             </div>
         </div>
     </section>
 @endsection
+
 @section('customJs')
+    <script>
+        // Initialize Stripe.js
+        var stripe = Stripe('{{ env('STRIPE_KEY') }}');
+        var elements = stripe.elements();
 
+        // Create an instance of the card Element
+        var card = elements.create('card');
 
+        // Add an instance of the card Element into the `card-number` div
+        card.mount('#card-number');
+
+        // Add event listener to handle form submission
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            // Disable the submit button to prevent multiple submissions
+            document.getElementById('card-button').disabled = true;
+
+            // Create payment method using the card Element
+            stripe.createPaymentMethod({
+                type: 'card',
+                card: card,
+                billing_details: {
+                    name: document.querySelector('input[name=name_on_card]').value
+                }
+            }).then(function (result) {
+                if (result.error) {
+                    // Show error to the customer
+                    showError(result.error.message);
+                } else {
+                    // Send paymentMethod.id to your server
+                    stripeTokenHandler(result.paymentMethod.id);
+                }
+            });
+        });
+
+        // Function to handle the token and submit form to the server
+        function stripeTokenHandler(paymentMethod) {
+            // Add paymentMethod as hidden input to the form
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type', 'hidden');
+            hiddenInput.setAttribute('name', 'stripePaymentMethod');
+            hiddenInput.setAttribute('value', paymentMethod);
+            form.appendChild(hiddenInput);
+
+            // Submit the form
+            form.submit();
+        }
+
+        // Function to display error message
+        function showError(errorMsgText) {
+            var errorDiv = document.createElement('div');
+            errorDiv.classList.add('alert', 'alert-danger');
+            errorDiv.textContent = errorMsgText;
+            var errorContainer = document.querySelector('.error');
+            errorContainer.appendChild(errorDiv);
+            setTimeout(function () {
+                errorContainer.removeChild(errorDiv);
+            }, 5000);
+            // Enable the submit button
+            document.getElementById('card-button').disabled = false;
+        }
+    </script>
 @endsection
